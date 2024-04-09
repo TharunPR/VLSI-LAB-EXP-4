47316ce6e27f73516e62303a7af8aa4e8c71e25f
Tharun <117915125+TharunPR@users.noreply.github.com>
2024-04-09 23:38:22 +0530

    Exp4_Tflipflop.v

--- Exp4_Tflipflop.v
+++ Exp4_Tflipflop.v
@@ -1,8 +1,13 @@
-6(iv):T FLIPFLOP 
-module tff(t,clk,q); 
-input t,clk; 
-output reg q=1; 
-
-
-
+module t(t,clk,rst,q);
+input t,clk,rst;
+output reg q;
+always @ (posedge clk)
+begin 
+if (rst==1)
+q=1'b0;
+else if (t==0)
+q=q;
+else
+q=~q;
+end
 endmodule
